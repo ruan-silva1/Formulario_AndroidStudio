0cdf56c61a19a490195aa7619c8a5783660c2276
ruan-silva1 <ruan.jdsilva@senacsp.edu.br>
2025-04-28 10:21:42 -0300

    adicionando formulario xml

---
+++ AndroidStudioProjects/Nitro/app/src/main/java/com/orgs/nitro/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,11 @@
+package com.orgs.nitro.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.orgs.nitro.R
+
+class FormularioProdutoActivity: AppCompatActivity(R.layout.formulario_produto_activity) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
